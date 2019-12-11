aac0ab927cf8e1a853872b381acbfd873fbc95f0
janhavi07 <partejanhavi@gmail.com>
2019-12-11 18:22:13 +0530

    Added Attributes in DAO Object

--- src/main/java/censusanalyser/IndiaCensusDAO.java
+++ src/main/java/censusanalyser/IndiaCensusDAO.java
@@ -1,4 +1,30 @@
 package censusanalyser;
 
+import java.util.Iterator;
+
 public class IndiaCensusDAO {
+
+    public String state;
+    public String StateCode;
+    public int population;
+    public int densityPerSqKm;
+    public int areaInSqKm;
+
+
+    public IndiaCensusDAO(IndiaCensusCSV csvFileIterator) {
+        state = csvFileIterator.state;
+        areaInSqKm=csvFileIterator.areaInSqKm;
+        densityPerSqKm=csvFileIterator.densityPerSqKm;
+        population=csvFileIterator.population;
+    }
+
+    @Override
+    public String toString() {
+        return "IndiaCensusDAO{" +
+                "population=" + population +
+                ", densityPerSqKm=" + densityPerSqKm +
+                ", areaInSqKm=" + areaInSqKm +
+                ", state='" + state + '\'' +
+                '}';
+    }
 }
