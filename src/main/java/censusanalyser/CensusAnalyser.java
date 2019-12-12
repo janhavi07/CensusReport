e99247d12bbef5107357c3b6d44799de86cb58d8
janhavi07 <partejanhavi@gmail.com>
2019-12-12 13:21:54 +0530

    Refactored the code by using Options

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -18,59 +18,14 @@ public class CensusAnalyser {
     public CensusAnalyser(){
         this.censusStateMap =new HashMap<>();
     }
-    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-        return this.loadCensusData(csvFilePath,IndiaCensusCSV.class);
+    public int loadIndiaCensusData(String... csvFilePath) throws CensusAnalyserException {
+        censusStateMap=new CensusLoader().loadCensusData(IndiaCensusCSV.class,csvFilePath[0]);
+        return censusStateMap.size();
     }
 
-    public int loadStateCode(String indianStateCode) throws CensusAnalyserException {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(indianStateCode));
-            ICSVBuilder icsvBuilder=CSVBuilderFactory.createCSVBuilder();
-            Iterator<IndiaStateCode> stateCodeIterator=icsvBuilder.getCSVFileIterator(reader,IndiaStateCode.class);
-            Iterable<IndiaStateCode> indiaStateCodeIterable=() -> stateCodeIterator;
-            StreamSupport.stream(indiaStateCodeIterable.spliterator(),false)
-                    .filter(csvState -> censusStateMap.get(csvState.StateName)!=null)
-                    .forEach(csvState -> censusStateMap.get(csvState.StateName).StateCode=csvState.StateCode);
-            return censusStateMap.size();
-        } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        } catch (RuntimeException e){
-            throw new CensusAnalyserException("Header Missing",
-                    CensusAnalyserException.ExceptionType.HEADER_INCORRECT_OR_INCORRECT_DELIMITER);
-        } catch (CSVBuilderException e) {
-            throw new CensusAnalyserException(e.getMessage(),e.type.name());
-        }
-    }
-
-    public int loadUSCensusData(String usCensusData) throws CensusAnalyserException {
-        return this.loadCensusData(usCensusData,USCensusCSV.class);
-    }
-
-    private <E> int loadCensusData(String csvFile,Class<E> censusCsvClass) throws CensusAnalyserException {
-        try {
-             Reader reader = Files.newBufferedReader(Paths.get(csvFile));
-             ICSVBuilder icsvBuilder= CSVBuilderFactory.createCSVBuilder();
-             Iterator<E> csvFileIterator=icsvBuilder.getCSVFileIterator(reader,censusCsvClass);
-             Iterable<E> csvIterable= () -> csvFileIterator;
-             if (censusCsvClass.getName().equals("censusanalyser.IndiaCensusCSV")){
-                        StreamSupport.stream(csvIterable.spliterator(), false).
-                        map(IndiaCensusCSV.class::cast).
-                        forEach(censusCSV -> censusStateMap.put(censusCSV.state, new CensusDAO(censusCSV)));
-             } else if(censusCsvClass.getName().equals("censusanalyser.USCensusCSV")) {
-                         StreamSupport.stream(csvIterable.spliterator(), false).
-                         map(USCensusCSV.class::cast).
-                         forEach(censusCSV -> censusStateMap.put(censusCSV.State, new CensusDAO(censusCSV)));
-             }
-            return censusStateMap.size();
-        } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        } catch (CSVBuilderException e) {
-            throw new CensusAnalyserException(e.getMessage(),e.type.name());
-        } catch (RuntimeException e) {
-            throw new CensusAnalyserException("Header missing", CensusAnalyserException.ExceptionType.HEADER_INCORRECT_OR_INCORRECT_DELIMITER);
-        }
+    public int loadUSCensusData(String... usCensusData) throws CensusAnalyserException {
+        censusStateMap=new CensusLoader().loadCensusData(USCensusCSV.class,usCensusData[0]);
+        return censusStateMap.size();
     }
 
     private String convertMapToList(Comparator<CensusDAO> censusCSVComparator) {
