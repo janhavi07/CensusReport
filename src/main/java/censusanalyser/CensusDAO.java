28d9f0af967a51c7b9d80521adbf17120e3b9c00
janhavi07 <partejanhavi@gmail.com>
2019-12-12 11:48:22 +0530

    Refactored the IndianCensusDAO to CensusDAO

--- src/main/java/censusanalyser/CensusDAO.java
+++ src/main/java/censusanalyser/CensusDAO.java
@@ -1,21 +1,28 @@
 package censusanalyser;
 
-import java.util.Iterator;
-
-public class IndiaCensusDAO {
+public class CensusDAO {
 
     public String state;
+    public int stateCodeUs;
     public String StateCode;
     public int population;
     public int densityPerSqKm;
     public int areaInSqKm;
 
-
-    public IndiaCensusDAO(IndiaCensusCSV csvFileIterator) {
+    public CensusDAO(IndiaCensusCSV csvFileIterator) {
         state = csvFileIterator.state;
         areaInSqKm=csvFileIterator.areaInSqKm;
         densityPerSqKm=csvFileIterator.densityPerSqKm;
         population=csvFileIterator.population;
+    }
+
+    public CensusDAO(USCensusCSV censusIterator){
+        state=censusIterator.State;
+        areaInSqKm= (int) censusIterator.Area;
+        densityPerSqKm= (int) censusIterator.populationDensity;
+       stateCodeUs=censusIterator.StateId;
+
+
     }
 
 }
