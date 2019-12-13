5c28e64a73b4720b9b448c1238eb636bff8223d4
janhavi07 <partejanhavi@gmail.com>
2019-12-13 18:43:56 +0530

    Refactored the sorting code to one getSortedString method

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -25,17 +25,6 @@ public class CensusAnalyser {
         return censusStateMap.size();
     }
 
-    public String getStateWiseSortedCensusData() {
-        this.toThrowNullException();
-        Comparator<CensusDAO> censusCSVComparator= Comparator.comparing(census -> census.state);
-        ArrayList censusDTO=censusStateMap.values().stream()
-                .sorted(censusCSVComparator)
-                .map(censusDAO -> censusDAO.getCensusDTO(country))
-                .collect(Collectors.toCollection(ArrayList::new));
-        String sortedStateCensusJson=new Gson().toJson(censusDTO);
-        return sortedStateCensusJson;
-    }
-
     private void toThrowNullException(){
         if( censusStateMap ==null || censusStateMap.size()==0){
             try {
@@ -46,7 +35,38 @@ public class CensusAnalyser {
         }
     }
 
+    public String getStateWiseSortedCensusData() {
+        this.toThrowNullException();
+        Comparator<CensusDAO> censusCSVComparator= Comparator.comparing(census -> census.state);
+        return this.getSortedString(censusCSVComparator);
+    }
 
+    public String getPopulationWiseSortedCensusData() {
+        this.toThrowNullException();
+        Comparator<CensusDAO> censusCSVComparator= Comparator.comparing(census -> census.population);
+        return this.getSortedString(censusCSVComparator);
+    }
 
+    public String getAreaWiseSortedCensusData() {
+        this.toThrowNullException();
+        Comparator<CensusDAO> censusCSVComparator= Comparator.comparing(census -> census.areaInSqKm);
+        return this.getSortedString(censusCSVComparator);
+    }
+
+    public String getDensityWiseSortedCensusData() {
+        this.toThrowNullException();
+        Comparator<CensusDAO> censusCSVComparator= Comparator.comparing(census -> census.densityPerSqKm);
+        return this.getSortedString(censusCSVComparator);
+
+    }
+
+    private String getSortedString(Comparator<CensusDAO> censusCSVComparator){
+        ArrayList censusDTO=censusStateMap.values().stream()
+                .sorted(censusCSVComparator)
+                .map(censusDAO -> censusDAO.getCensusDTO(country))
+                .collect(Collectors.toCollection(ArrayList::new));
+        String sortedStateCensusJson=new Gson().toJson(censusDTO);
+        return sortedStateCensusJson;
+    }
 
 }
